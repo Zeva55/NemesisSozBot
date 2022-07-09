c10998bec4f2bf9aafc7d2326fd2ae94f908d350
RowLyn <95884044+RowLyn@users.noreply.github.com>
2022-07-09 15:16:31 +0400

    Update rating_si.py

--- kelime_bot/plugins/rating_si.py
+++ kelime_bot/plugins/rating_si.py
@@ -1,10 +1,10 @@
-from kelime_bot import reytinq
+from kelime_bot import rating
 from pyrogram import Client
 from pyrogram import filters
 from pyrogram.types import Message
 
 
-@Client.on_message(filters.command("reytinq"))
+@Client.on_message(filters.command("rating"))
 async def ratingsa(c:Client, m:Message):
     global rating
     metin = """📝 Qlobal Qrup Reytinqi :
