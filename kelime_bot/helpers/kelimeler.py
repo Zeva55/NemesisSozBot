6bb50618f94a4d9b2da48edb05f2ea709bbe7d57
RowLyn <95884044+RowLyn@users.noreply.github.com>
2022-07-10 02:08:42 +0400

    Update kelimeler.py

--- kelime_bot/helpers/kelimeler.py
+++ kelime_bot/helpers/kelimeler.py
@@ -3,9 +3,8 @@ kelimeler = ["ailə", "ailəli", "əzab", "əzbərləmək", "əzgil",
              "əziyyət", "əziz", "əzizləmək","əzmək","əzələ", "əzəmət",     
              "əzəmətli", "əş", "sus", "susmaq", "istedad", "istefa","istehlak",  
              "istehsal", "Gələcək", "Əsəbləşmək", "Yemək", "Gecə", "Səhər", 
+             ]
 
 
-
-
-
-
+def kelime_sec():
+    return random.choice(kelimeler)
