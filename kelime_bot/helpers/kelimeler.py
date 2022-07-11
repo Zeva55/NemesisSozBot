e940aa828257bdd36b0a2474af68317ddd0ec18e
RowLyn <95884044+RowLyn@users.noreply.github.com>
2022-07-11 05:01:43 +0400

    Update kelimeler.py

--- kelime_bot/helpers/kelimeler.py
+++ kelime_bot/helpers/kelimeler.py
@@ -1,8 +1,13 @@
 import random
 kelimeler = ["ailə", "ailəli", "əzab", "əzbərləmək", "əzgil",
-             "əziyyət", "əziz", "əzizləmək","əzmək","əzələ", "əzəmət",     
-             "əzəmətli", "əş", "sus", "susmaq", "istedad", "istefa","istehlak",  
-             "istehsal", "Gələcək", "Əsəbləşmək", "Yemək", "Gecə", "Səhər"
+             "əziyyət", "əziz", "əzizləmək", "əzmək", "əzələ", "əzəmət",     
+             "əzəmətli", "iş", "sus", "susmaq", "istedad", "istefa","istehlak",  
+             "istehsal", "gələcək", "əsəbləşmək", "yemək", "gecə", "səhər", "ölmək",
+             "ayılmaq", "ayrılmaq", "sözsüz", "xonça", "əvəzedilməz", "ömür", "səbrsiz"
+             'kəlmə', 'sual', 'əmr', 'radyo', 'götürmək', 'sərhəd', 'sərhədsiz', 'üzülmək',   
+             'darıxmaq', 'külək', 'sənəd', 'sümük', 'sahibsiz', 'vətən', 'əməliyyat', 'əziyyət',  
+             'söyüş', 'meyxana', 'istəmək', 'danışmaq', 'gülmək', 'azərbaycanlı', 'namaz', 'görüşmək',  
+             'mahnı', 'restoran', 'riyaziyyat', 'səhvsiz', 'səməni', 'həyat', 'küçələr', 'yerimək', 'televizor', 
              ]
 
 
